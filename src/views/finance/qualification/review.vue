5a2a77a92fc2a39c6953a381be21b478fe71918b
yyk <y85619704@qq.com>
2026-08-09 17:00:13 +0800

    feat: 修改bug

--- src/views/finance/qualification/review.vue
+++ src/views/finance/qualification/review.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-// Map the backend menu path to the existing qualification-review workbench.
-import SubAccountReview from '@/views/recruitment/sub-account/index.vue';
+// Map the backend menu path to the finance-center review workbench.
+import FinanceTaskReview from '@/views/recruitment/finance-task.vue';
 </script>
 
 <template>
-  <SubAccountReview />
+  <FinanceTaskReview />
 </template>
